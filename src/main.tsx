c5d1d4c7a8667f6fe8fc5eaa8087343a3747ecde
guiguik12 <guigui_pvh@hotmail.com>
2026-03-25 23:31:34 -0400

    falha nos imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './tailwind.css'  // ou './styles.css' / './App.css' — onde tá seu Tailwind
+import App from './app/App.tsx'  // ← CAMINHO CORRETO agora!
+import './styles/tailwind.css'  // Tailwind também
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <React.StrictMode>
